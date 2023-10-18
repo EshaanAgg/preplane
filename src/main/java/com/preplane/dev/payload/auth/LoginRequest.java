39759a9fdb8956caefcb892c316aba68d213592b
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-10-18 16:44:13 +0530

    feat: add login and redirect to the home page

--- src/main/java/com/preplane/dev/payload/auth/LoginRequest.java
+++ src/main/java/com/preplane/dev/payload/auth/LoginRequest.java
@@ -3,9 +3,9 @@ package com.preplane.dev.payload.auth;
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
-    @NotBlank(message = "'username' is a required property")
+    @NotBlank(message = "Username can't be blank.")
     public String username;
 
-    @NotBlank(message = "'Password' is a required property")
+    @NotBlank(message = "Password cannot be blank.")
     public String password;
 }
