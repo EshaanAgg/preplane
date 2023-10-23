986a1df2d9153b0632ad2d8473d37d3d4a115235
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-10-23 09:45:12 +0530

    Integrate Razorpay Payment Gateway

---
+++ src/main/java/com/preplane/dev/controllers/backend/PaymentController.java
@@ -0,0 +1,38 @@
+package com.preplane.dev.controllers.backend;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.razorpay.*;
+
+@RestController
+public class PaymentController {
+
+    @Value("${razorpay.keyId}")
+    private String apiKey;
+
+    @Value("${razorpay.keySecret}")
+    private String apiSecret;
+
+    @GetMapping("/payment/createOrderId/{amount}")
+    @ResponseBody
+    public String createPaymentOrderId(@PathVariable String amount) {
+        String orderId=null;
+        try {
+            RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
+            JSONObject orderRequest = new JSONObject();
+            orderRequest.put("amount", amount); // amount in the smallest currency unit
+            orderRequest.put("currency", "INR");
+            orderRequest.put("receipt", "orderReceiptID");
+
+            Order order = razorpay.orders.create(orderRequest);
+            orderId = order.get("id");
+        } catch (RazorpayException e) {
+            System.out.println(e.getMessage());
+        }
+        return orderId;
+    }
+}
--- src/main/java/com/preplane/dev/controllers/frontend/OtherControllers.java
+++ src/main/java/com/preplane/dev/controllers/frontend/OtherControllers.java
@@ -2,6 +2,7 @@ package com.preplane.dev.controllers.frontend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OtherControllers {
@@ -23,5 +24,9 @@ public class OtherControllers {
         return "blog/blog";
     }
 
+    @PostMapping("/payment/success")
+    public String paymentSuccess() {
+        return "premium/success";
+    }
     
 }
