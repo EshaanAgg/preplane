2b8530f4cfe119e578e409aa05b40bc30df7eeea
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-10-23 11:02:11 +0530

    Redirect to error page

--- src/main/java/com/preplane/dev/controllers/backend/PaymentController.java
+++ src/main/java/com/preplane/dev/controllers/backend/PaymentController.java
@@ -2,6 +2,8 @@ package com.preplane.dev.controllers.backend;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +21,7 @@ public class PaymentController {
 
     @GetMapping("/payment/createOrderId/{amount}")
     @ResponseBody
-    public String createPaymentOrderId(@PathVariable String amount) {
+    public String createPaymentOrderId(@PathVariable String amount, Model model) {
         String orderId=null;
         try {
             RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
@@ -32,7 +34,16 @@ public class PaymentController {
             orderId = order.get("id");
         } catch (RazorpayException e) {
             System.out.println(e.getMessage());
+            handleRazorpayException(e, model);
+            return "error/paymentError";
         }
         return orderId;
     }
+
+    @ExceptionHandler(RazorpayException.class)
+    public String handleRazorpayException(RazorpayException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/paymentError"; // Redirect to error page
+    }
+
 }
