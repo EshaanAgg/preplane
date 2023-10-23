83dbe6fec3fb95c6154884250cc8c7d00248775f
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-10-23 13:33:50 +0530

    resolve

--- src/main/java/com/preplane/dev/controllers/frontend/OtherControllers.java
+++ src/main/java/com/preplane/dev/controllers/frontend/OtherControllers.java
@@ -3,6 +3,7 @@ package com.preplane.dev.controllers.frontend;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class OtherControllers {
